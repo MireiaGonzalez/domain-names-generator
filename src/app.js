0719eec341f58b1c275fc07ff1e46780b02f9653
MireiaGonzalez <mireiagonzalezz03@gmail.com>
2021-12-13 19:11:22 +0000

    exercise

--- src/app.js
+++ src/app.js
@@ -4,8 +4,25 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { name } from "file-loader";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["your", "the", "my"];
+  let adj = ["cool", "best", "cute", "strong"];
+  let noun = ["house", "dog", "box"];
+
+  let nameArr = [];
+
+  for (let i in pronoun) {
+    for (let j in adj) {
+      for (let g in noun) {
+        nameArr.push(pronoun[i] + adj[j] + noun[g] + ".com <br>");
+      }
+    }
+  }
+
+  console.log(nameArr);
+
+  const root = document.querySelector("#root");
+  root.innerHTML = nameArr;
 };
